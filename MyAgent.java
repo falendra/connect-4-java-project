f938d895ceb6cbfdff76db9f7bdc3a62807a066e
Falendra Bandhe <36308875+falendra@users.noreply.github.com>
2018-09-16 23:46:36 +0530

    Add files via upload

--- MyAgent.java
+++ MyAgent.java
@@ -1,154 +1,111 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class MyAgent extends Agent
 {
     Random r;
-
-    /**
-     * Constructs a new agent, giving it the game and telling it whether it is Red or Yellow.
-     * 
-     * @param game The game the agent will be playing.
-     * @param iAmRed True if the agent is Red, False if the agent is Yellow.
-     */
-    public MyAgent(Connect4Game game, boolean iAmRed)
-    {
+    public MyAgent(Connect4Game game, boolean iAmRed) {
         super(game, iAmRed);
-        r = new Random();
+        r=new Random();
     }
-
-    /**
-     * The move method is run every time it is this agent's turn in the game. You may assume that
-     * when move() is called, the game has at least one open slot for a token, and the game has not
-     * already been won.
-     * 
-     * By the end of the move method, the agent should have placed one token into the game at some
-     * point.
-     * 
-     * After the move() method is called, the game engine will check to make sure the move was
-     * valid. A move might be invalid if:
-     * - No token was place into the game.
-     * - More than one token was placed into the game.
-     * - A previous token was removed from the game.
-     * - The color of a previous token was changed.
-     * - There are empty spaces below where the token was placed.
-     * 
-     * If an invalid move is made, the game engine will announce it and the game will be ended.
-     * 
-     */
-    public void move()
-    {
-        //int columnNumber =randomMove();
-        Scanner sc = new Scanner(System.in);
-        
-        int columnNumber =sc.nextInt();
-        
-        System.out.println(columnNumber);
-        
-        moveOnColumn(columnNumber);
-      
-    }
-
-    /**
-     * Drops a token into a particular column so that it will fall to the bottom of the column.
-     * If the column is already full, nothing will change.
-     * 
-     * @param columnNumber The column into which to drop the token.
-     */
-    public void moveOnColumn(int columnNumber)
-    {
-        int lowestEmptySlotIndex = getLowestEmptyIndex(myGame.getColumn(columnNumber));   // Find the top empty slot in the column
-                                                                                                  // If the column is full, lowestEmptySlot will be -1
-        if (lowestEmptySlotIndex > -1)  // if the column is not full
-        {
-            Connect4Slot lowestEmptySlot = myGame.getColumn(columnNumber).getSlot(lowestEmptySlotIndex);  // get the slot in this column at this index
-            if (iAmRed) // If the current agent is the Red player...
-            {
-                lowestEmptySlot.addRed(); // Place a red token into the empty slot
-            }
-            else // If the current agent is the Yellow player (not the Red player)...
-            {
-                lowestEmptySlot.addYellow(); // Place a yellow token into the empty slot
-            }
+    
+    public void move() {
+        int move = 0;
+        //////System.out.println(toString() + "Moving...");
+        int iCanWin = canWin(iAmRed);
+        int theyCanWin = canWin(!iAmRed);
+        if(iCanWin >= 0) {
+            move=iCanWin;
+            //System.out.println(toString() + ": I can win! Moving at " + move + ".");
+        } else if(theyCanWin >= 0) {
+            move=theyCanWin;
+            //System.out.println(toString() + ": They can win! Moving at " + move + ".");
+        } else {
+            move=randomMove();
+            //System.out.println(toString() + ": Move randomly! Moving at " + move + ".");
         }
+        
+        moveOnColumn(move);
     }
-
-    /**
-     * Returns the index of the top empty slot in a particular column.
-     * 
-     * @param column The column to check.
-     * @return the index of the top empty slot in a particular column; -1 if the column is already full.
-     */
-    public int getLowestEmptyIndex(Connect4Column column) {
-        int lowestEmptySlot = -1;
-        for  (int i = 0; i < column.getRowCount(); i++)
-        {
-            if (!column.getSlot(i).getIsFilled())
-            {
-                lowestEmptySlot = i;
-            }
-        }
-        return lowestEmptySlot;
-    }
-
-    /**
-     * Returns a random valid move. If your agent doesn't know what to do, making a random move
-     * can allow the game to go on anyway.
-     * 
-     * @return a random valid move.
-     */
-    public int randomMove()
-    {
+    
+    public int randomMove() {
         int i = r.nextInt(myGame.getColumnCount());
-        while (getLowestEmptyIndex(myGame.getColumn(i)) == -1)
-        {
+        while(getTopEmptySlot(myGame.getColumn(i))==null) {
             i = r.nextInt(myGame.getColumnCount());
         }
         return i;
     }
-
-    /**
-     * Returns the column that would allow the agent to win.
-     * 
-     * You might want your agent to check to see if it has a winning move available to it so that
-     * it can go ahead and make that move. Implement this method to return what column would
-     * allow the agent to win.
-     *
-     * @return the column that would allow the agent to win.
-     */
-    public int iCanWin()
-    {
-        return 0;
+    public void moveOnColumn(int i) {
+        Connect4Slot topEmptySlot = getTopEmptySlot(myGame.getColumn(i));
+        if(!(topEmptySlot==null)) {
+            if(iAmRed) {
+                topEmptySlot.addRed();
+            } else {
+                topEmptySlot.addYellow();
+            }
+        }
     }
-
-    /**
-     * Returns the column that would allow the opponent to win.
-     * 
-     * You might want your agent to check to see if the opponent would have any winning moves
-     * available so your agent can block them. Implement this method to return what column should
-     * be blocked to prevent the opponent from winning.
-     *
-     * @return the column that would allow the opponent to win.
-     */
-    public int theyCanWin()
-    {
-        return 0;
+    public Connect4Slot getTopEmptySlot(Connect4Column column) {
+        int topEmptySlot=-1;
+        for(int i = 0; i<column.getRowCount(); i++) {
+            if(!column.getSlot(i).getIsFilled()) {
+                topEmptySlot=i;
+            }
+        }
+        if(topEmptySlot<0) {
+            return null;
+        } else {
+            return column.getSlot(topEmptySlot);
+        }
     }
-
-    /**
-     * Returns the name of this agent.
-     *
-     * @return the agent's name
-     * 
-     */
     
-    public String getName()
-    {
+    public int canWin(boolean red) {
+        ////System.out.println("  Checking can win...");
+        for(int i = 0; i < myGame.getColumnCount(); i++) {
+            ////System.out.println("    Checking column " + i + "...");
+            int tei = getTopEmptyIndex(myGame.getColumn(i));
+            if(tei > -1) {
+                ////System.out.println("    Column " + i + " has an empty slot at " + tei + ".");
+                if(tei < myGame.getRowCount() - 3) { // if more than 3 tokens might be below this slot
+                    ////System.out.println("      Checking if there's three tokens below...");
+                    if(myGame.getColumn(i).getSlot(tei+1).getIsRed()==red && myGame.getColumn(i).getSlot(tei+2).getIsRed()==red && myGame.getColumn(i).getSlot(tei+3).getIsRed()==red) { // if a column win is available here
+                        return i;
+                    }
+                }
+                if(i < myGame.getColumnCount() - 3) { // if three tokens might be to the right
+                    ////System.out.println("      Checking if there's three tokens to the right...");
+                    if(checkIfEqual(red, myGame.getColumn(i+1).getSlot(tei), myGame.getColumn(i+2).getSlot(tei), myGame.getColumn(i+3).getSlot(tei))) {
+                        return i;
+                    }
+                }
+                if(i > 2) { // if three tokens might be to the left
+                    ////System.out.println("      Checking if there's three tokens to the left...");
+                    if(checkIfEqual(red, myGame.getColumn(i-1).getSlot(tei), myGame.getColumn(i-3).getSlot(tei), myGame.getColumn(i-2).getSlot(tei))) {
+                        return i;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    public boolean checkIfEqual(boolean isRed, Connect4Slot slot1, Connect4Slot slot2, Connect4Slot slot3) {
+        if(slot1.getIsFilled() && slot2.getIsFilled() && slot3.getIsFilled()) {
+            if(slot1.getIsRed()==isRed && slot2.getIsRed()==isRed && slot3.getIsRed()==isRed) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public int getTopEmptyIndex(Connect4Column column) {
+        int topEmptySlot=-1;
+        for(int i = 0; i<column.getRowCount(); i++) {
+            if(!column.getSlot(i).getIsFilled()) {
+                topEmptySlot=i;
+            }
+        }
+        return topEmptySlot;
+    }
+    
+    public String getName() {
         return "My Agent";
     }
-    
-   
-    
-    
-
 }
